9cc615a320083b157f196fd904a36bf3f63cfa8f
DAVID SILVENTE GALERA <dsg503@inlumine.ual.es>
2023-04-18 18:42:42 +0200

    Test requisito y stakeholder, creacion carpetas src, test y cambios en solucion y algoritmo

---
+++ test/test_stakeholder.py
@@ -0,0 +1,21 @@
+import unittest
+from stakeholder import Stakeholder
+
+class TestStakeholder(unittest.TestCase):
+
+    def setUp(self):
+        self.stakeholder1 = Stakeholder("Stakeholder 1")
+        self.stakeholder2 = Stakeholder("Stakeholder 2")
+
+    def test_agregar_recomendacion(self):
+        self.stakeholder1.agregar_recomendacion(self.stakeholder2)
+        self.assertEqual(self.stakeholder1.recomendaciones, 1)
+
+    def test_calcular_importancia(self):
+        self.stakeholder1.agregar_recomendacion(self.stakeholder2)
+        self.stakeholder2.agregar_recomendacion(self.stakeholder1)
+        self.assertEqual(self.stakeholder1.calcular_importancia(), 1)
+        self.assertEqual(self.stakeholder2.calcular_importancia(), 1)
+
+if __name__ == '__main__':
+    unittest.main()
